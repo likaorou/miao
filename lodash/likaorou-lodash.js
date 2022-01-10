8e1d4a2a433cdc4cd2e0af1a6d87912671958a80
likaorou <774897985@qq.com>
2022-01-10 16:53:32 +0800

    lodash

--- lodash/likaorou-lodash.js
+++ lodash/likaorou-lodash.js
@@ -5,24 +5,63 @@ var likaorou = {
       result.push(array)
       return result
     } else {
-      let count = 0
-      let countArray = []
+      let count = []
       for (let i = 0; i <= array.length; i++) {
-        if (count < size && i < array.length) {
-          countArray.push(array[i])
-          count++
-        } else if (count == size) {
-          result.push(countArray)
-          countArray = []
-          count = 0
-          i--
-        } else if (i == array.length) {
-          result.push(countArray)
-          countArray = []
-          count = 0
+        if (count.length == size) {
+          result.push(count)
+          count = []
+        } else {
+          count.push(array[i])
         }
+      }
+      if (count.length !== 0) {
+        result.push(count)
       }
       return result
     }
   },
+  compact: function (array) {
+    let result = []
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] == 0 || array[i] == undefined || array[i] == '' || array[i] == false || array[i] == null || array[i] !== array[i]) {
+        continue
+      } else {
+        result.push(array[i])
+      }
+    }
+    return result
+  },
+  drop: function (array, n = 1) {
+    let result = []
+    for (let i = n; i < array.length; i++) {
+      result.push(array[i])
+    }
+    array = result
+    return array
+  },
+  dropRight: function (array, n = 1) {
+    let result = []
+    for (let i = 0; i + n < array.length; i++) {
+      result.push(array[i])
+    }
+    return array = result
+  },
+  dropRightWhile: function () { },
+  dropWhile: function () { },
+  fill: function () { },
+  findIndex: function () { },
+  findLastIndex: function () { },
+  flatten: function () { },
+  flattenDeep: function () { },
+  flattenDepth: function () { },
+  compact: function () { },
+  compact: function () { },
+  compact: function () { },
+  compact: function () { },
+  compact: function () { },
+  compact: function () { },
+  compact: function () { },
+  compact: function () { },
+
+
 }

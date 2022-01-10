6c2403724821bcd2853f67c7dda962459393c9f9
likaorou <774897985@qq.com>
2022-01-10 16:14:29 +0800

    lodash

--- lodash/likaorou-lodash.js
+++ lodash/likaorou-lodash.js
@@ -1,3 +1,28 @@
 var likaorou = {
-  chunk: function () { },
+  chunk: function (array, size = 1) {
+    let result = []
+    if (array.length < size) {
+      result.push(array)
+      return result
+    } else {
+      let count = 0
+      let countArray = []
+      for (let i = 0; i <= array.length; i++) {
+        if (count < size && i < array.length) {
+          countArray.push(array[i])
+          count++
+        } else if (count == size) {
+          result.push(countArray)
+          countArray = []
+          count = 0
+          i--
+        } else if (i == array.length) {
+          result.push(countArray)
+          countArray = []
+          count = 0
+        }
+      }
+      return result
+    }
+  },
 }
